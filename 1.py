a0a140c603a8181bdcd5bf2c72c27025a2d655fa
jacko8638 <jdowning@kingsworcester.org.uk>
2018-02-07 01:10:14 +0000

    yeah

--- 1.py
+++ 1.py
@@ -1,9 +1,14 @@
+#gets inputs
 Kilo = input("enter kilos")
 pounds = input("enter pounds")
 choice = input("which would you like to convert into the other?")
+
+#if statement to find if pounds or kilos
 if choice == "kilo":
+    #converts kilo to pounds
     answer = int(Kilo) / 2.20462218
     print(answer)
 elif choice == "pounds":
+    #converts pounds to kilos
     answer = int(pounds) * 2.20462218
     print(answer)
--- 2.py
+++ 2.py
@@ -1,7 +1,12 @@
+#gets user to input num
 number = input("enter a three digit number")
+#finds how many hundreds there are in it as n int
 hund = int(int(number) / 100)
+#using indexing finds how many tens
 tens = number[1]
+#using indexing finds how many units
 units = number[2]
+#prints results
 print("there are", hund," hundreds")
 print("there are", tens," tens")
 print("there are", units," units")
